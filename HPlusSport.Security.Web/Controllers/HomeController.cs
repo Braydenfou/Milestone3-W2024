99be4b3e979d711fc98ab0d123df081fac5068be
Brayden Fournier <6194481@johnabbottcollege.net>
2024-11-15 16:31:05 -0500

    Cleaned 1 + 2, Added HomeController header.

--- HPlusSport.Security.Web/Controllers/HomeController.cs
+++ HPlusSport.Security.Web/Controllers/HomeController.cs
@@ -2,6 +2,15 @@ using HPlusSport.Security.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
+/*Course: 		Web Programming 3
+* Assessment: 	Milestone 3
+* Created by: 	Brayden Fournier - 6194481
+* Date: 		15/11/2024
+* Class Name: 	HomeController.cs
+* Description: 	This controller holds all main logic for returning the main view aswell as an error view in the case of errors.
+* Time Task B):	Ch 1 + 2: 1 Hour.
+*/
+
 namespace HPlusSport.Security.Web.Controllers;
 
 public class HomeController : Controller
